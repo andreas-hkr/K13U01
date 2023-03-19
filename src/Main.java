4a73875303b28d979a37b96501d137cc0e5b8475
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-19 09:38:03 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // - Skapa en klass Book som kan lagra titel, författare, sidantal och pris
+        // - Skapa två objekt:
+        //     Java - Steg för steg, Jan Skansholm, 483 sidor, 400kr
+        //     Starting out with Python, Tony Gaddis, 896 sidor, 800kr
+
+
+    }
+}
